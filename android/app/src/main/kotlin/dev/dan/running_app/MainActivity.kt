612ea50fc2fb8a8691a1836889e656cf5a80b890
DaniyalAkhtar <daniyalakhtarpatoojo125@gmail.com>
2026-07-19 08:45:19 -0700

    Updated Icon & Name

---
+++ android/app/src/main/kotlin/dev/dan/running_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.dan.running_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/Ui/Splash Screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Splash_screen extends StatelessWidget {
+  const Splash_screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
