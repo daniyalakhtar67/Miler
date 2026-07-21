cd81c1c0779c5f2a78bd7ace29663a6dbca20e10
DaniyalAkhtar <daniyalakhtarpatoojo125@gmail.com>
2026-07-21 23:29:59 +0500

    Working On SingUp Screen

--- lib/ui/signup.dart
+++ lib/ui/signup.dart
@@ -1,4 +1,9 @@
+import 'dart:math';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:running_app/ui/login.dart';
+import 'package:running_app/widgets/colors.dart';
 
 class Signup extends StatefulWidget {
   const Signup({super.key});
@@ -8,8 +13,46 @@ class Signup extends StatefulWidget {
 }
 
 class _SignupState extends State<Signup> {
+
+
+  final email = TextEditingController();
+  final pass = TextEditingController();
+  bool loading = true;
+  @override
+  void dispose() {
+    // TODO: implement initState
+    super.dispose();
+    email.dispose();
+    pass.dispose();
+  }
+  Widget _blog(double size, Color color){
+    return ImageFiltered(imageFilter: ImageFilter.blur(sigmaX: 80, sigmaY: 80),
+    child: Container(
+        height: size,
+        width: size,
+      decoration: BoxDecoration(color: color, shape: BoxShape.circle),
+    ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+
+    return Scaffold(
+      // appBar: AppBar(
+      //   leading: GestureDetector(
+      //     onTap: ()=> Navigator.push(context, MaterialPageRoute(builder: (context) => Login())),
+      //   ),
+      //
+      // ),
+        body: Stack(
+          children: [
+            Positioned.fill(child: Container(color: AppColors.backgroundDark)),
+            Positioned(top: -50, bottom: -50, child: _blog(200, AppColors.new1)),
+            Positioned(top: 100, bottom: -60, child: _blog(220, AppColors.new2)),
+            Positioned(top:-60, bottom: 60, child: _blog(200, AppColors.new3)),
+          ],
+        ),
+    );
   }
 }
